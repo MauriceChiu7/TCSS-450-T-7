97d6b115e609b75c1b814362c868bef914abbe91
mauricechiu7 <morisbroderick@gmail.com>
2019-01-16 20:19:33 -0800

    empty project

---
+++ README.md
@@ -0,0 +1,4 @@
+# TCSS-450-T7
+Mobile App Project
+
+Members: Samantha Evelynn Anderson, Giovanni Cheng Tang, Luke Gillmore, Maurice Chiu
---
+++ app/src/main/java/tcss450/uw/edu/tcss_450_t7/MainActivity.java
@@ -0,0 +1,13 @@
+package tcss450.uw.edu.tcss_450_t7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
