10d185437d3e7d33009b86518e2cb205a905b652
Luke <lucasgillmore@yahoo.com>
2019-01-19 18:58:29 -0800

    Added comment on main activity

--- app/src/main/java/tcss450/uw/edu/tcss_450_t7/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/tcss_450_t7/MainActivity.java
@@ -1,10 +1,16 @@
 package tcss450.uw.edu.tcss_450_t7;
+/**
+ * Luke added comment for git testing.
+ */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
