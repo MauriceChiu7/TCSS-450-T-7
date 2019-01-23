5cc9b472dcc3c8641690cc33f4271baf18ce9158
Giovanni <bestgio@uw.edu>
2019-01-22 19:27:16 -0800

    Added one comment in MainActivity to test committing and pushing features.

--- app/src/main/java/tcss450/uw/edu/tcss_450_t7/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/tcss_450_t7/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Gio was here!
         setContentView(R.layout.activity_main);
     }
 }
